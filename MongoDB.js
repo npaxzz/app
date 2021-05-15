a0f599b766c8e4fca2961d4e876f536f3327bca3
npaxzz <82027125+npaxzz@users.noreply.github.com>
2021-05-15 16:37:07 +0700

    test get => all

--- MongoDB.js
+++ MongoDB.js
@@ -6,7 +6,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.all("/", (req, res) => {
   res.render("MongoDB");
 });
 
