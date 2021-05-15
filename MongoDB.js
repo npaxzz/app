144016765ed7a518139ae72125c25801f42c09db
npaxzz <82027125+npaxzz@users.noreply.github.com>
2021-05-15 14:48:40 +0700

    port => 3000

--- MongoDB.js
+++ MongoDB.js
@@ -10,5 +10,5 @@ app.get("/", (req, res) => {
   res.render("MongoDB");
 });
 
-app.listen(27017);
-console.log("Server started on port : 27017");
+app.listen(3000);
+console.log("Server started on port : 3000");
